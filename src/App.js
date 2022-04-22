b620a8d977056278c70d3653ba6a61439b0ed484
Macbright007 <nbright239@gmail.com>
2022-04-22 16:01:06 +0100

    updated functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import BoardButton from './components/BoardButton';
 import Screen from './components/Screen';
 
 const App = () => {
 
+  const [output, setOutput] = useState("0");
+
+  const HandleSolution = (v) => {
+    console.log(v);
+    setOutput(`${output === '0' ? '' : output} ${v}`)
+  }
+   
+  const handleSubmit = () =>{
+    {setOutput(eval(output))  && setOutput("")}
+  }
+  const resetHandle = () => {
+    setOutput("0")
+  }
+
   return (
     <div className="App">
       <h1>Calculator</h1>
-      <Screen />
-      <BoardButton />
+      <Screen output={output}/>
+      <BoardButton HandleSolution={HandleSolution} resetHandle={resetHandle} handleSubmit={handleSubmit} />
     </div>
   );
 }
